1d63475845d66a525b761d20f51c7e51dbf996e2
provash10 <provash20cb@gmail.com>
2024-10-21 21:42:38 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let indexArray = [43, 98, 34, 56, 33, 65];
+
+indexArray[4];
+console.log(indexArray[4]);
+console.log(indexArray[0]);
+console.log(indexArray[5]);
+console.log(indexArray[2]);
+
+
+// 3. Associative Array (Object)
+
+let student ={name: "Rahim", age: 32, blood: "O+"}
+console.log(student.age);
+console.log(student.name);
+
+// 2D Array 
+
+let twoDimensionalArray = [
+    [43, 98, 32, 2],
+    [11, 81, 39, 47],
+    [12, 5, 100, 77],
+    [91, 1,   2,  0],
+];
+
+console.log(twoDimensionalArray[0][0]);
+console.log(twoDimensionalArray[0][1]);
+console.log(twoDimensionalArray[0][2]);
+console.log(twoDimensionalArray[0][3]);
+
+console.log(twoDimensionalArray[1][0]);
+console.log(twoDimensionalArray[1][1]);
+console.log(twoDimensionalArray[1][2]);
+console.log(twoDimensionalArray[1][3]);
